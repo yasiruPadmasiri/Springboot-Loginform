199506d4e9821c7683a7f9578ca4609d68a7774f
yasiruPadmasiri <66236929+yasiruPadmasiri@users.noreply.github.com>
2020-11-09 16:45:20 +0530

    start

---
+++ Loginform/src/test/java/com/example/Loginform/LoginformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Loginform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
